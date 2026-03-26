ea0965d9a2a20790a06d3a749d229526c33aea15
kirill <kirill56gaponenko@gmail.com>
2026-03-26 21:00:05 +0200

    add integration test for studentController

--- src/service/studentService.js
+++ src/service/studentService.js
@@ -13,7 +13,7 @@ export const findStudent = async (id) => await repo.findStudentById(id);
 export const deleteStudent = async (id) => await repo.deleteStudentById(id);
 
 export const updateStudent = async (id, data) => {
-    const student = (await repo.updateStudent(id, data)).toObject();
+    const student = (await repo.updateStudent(id, data))?.toObject();
     if (student) {
         student.scores = undefined;
     }
